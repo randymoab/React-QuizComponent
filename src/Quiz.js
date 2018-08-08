deed0be3c850abb755f27734906ad80abb2bc567
Randy Holland <randall.holland@ldschurch.org>
2018-08-08 13:30:13 -0600

    changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
     render(){
-        const isQuizEnd = () => ((this.state.quiz_position-1) === quizData.quiz_questions.length) ? 'True' : 'False'
+        const isQuizEnd = false//() => ((this.state.quiz_position-1) === quizData.quiz_questions.length) ? 'True' : 'false'
         if (isQuizEnd) {
             return (
                 <div>
